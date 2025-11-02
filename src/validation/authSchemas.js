bb0eff6b009e007889e9ac2fb67e4b3bc7aa1b14
hamza7p_auto <mhamadhamzaalattar.75@gmail.com>
2025-11-02 09:47:06 +0300

    margin-footer

--- src/validation/authSchemas.js
+++ src/validation/authSchemas.js
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 export const loginSchema = Yup.object({
-  email: Yup.string().email("Enter a valid email address").required("Email is required"),
+  email: Yup.string().required("Your email or username is required"),
   password: Yup.string()
     .min(8, "Password must be at least 8 characters")
     .required("Password is required"),
